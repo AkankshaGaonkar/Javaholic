140258c18fcba12d1979ffaa526408ef75ca7ff8
Prasad Acharya <50115378+prasad145@users.noreply.github.com>
2022-04-13 02:04:42 +0530

    fixed some minor issues!!! i love u so much

--- src/MultiplesOfThreeOrFive.java
+++ src/MultiplesOfThreeOrFive.java
@@ -6,16 +6,16 @@ public class MultiplesOfThreeOrFive {
         Scanner num = new Scanner(System.in);
         System.out.println("Enter the number:");
         int n = num.nextInt();
-        int sum=0;
-        for (int no = 0; no < n; no++)
+        int sum = 0;
+        for (int no = 1; no <= n; no++)
         {
             if (no % 3 == 0 || no % 5 == 0)
             {
                 System.out.print(no + " ");
-                sum=sum+no;
+                sum = sum + no;
             }
         }
         System.out.println("\nThe sum of the numbers that are divisible by 3 or 5 is "+sum);
-        }
+       }
 }
 

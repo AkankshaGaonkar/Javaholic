cdcdf47a437c8257a14d7ce122d08d7d5aebcc75
AkankshaGaonkar <ashugaonkar2000@gmail.com>
2022-04-12 18:54:53 +0530

    SumOfNumUsingFormula Program

--- src/MultiplesOfThreeOrFive.java
+++ src/MultiplesOfThreeOrFive.java
@@ -1,24 +1,19 @@
 import java.util.Scanner;
 
 public class MultiplesOfThreeOrFive {
-    public static void main (String args[])
+    public static void main(String args[])
     {
         Scanner num = new Scanner(System.in);
         System.out.println("Enter the number:");
-        int number = num.nextInt();
+        int n = num.nextInt();
         int sum=0;
-        for(int i=1;i<=number;i++)
+        for (int i = 1; i <= n; i++)
         {
-            sum = sum + i;
+            sum=sum+i;
+            if(sum % 3 == 0 || sum % 5 ==0)
+            {
+                System.out.println("The number that is divisible by 3 or 5 are " + sum);
+            }
         }
-        if(sum%3==0)
-        {
-            System.out.println("The Sum of is "+sum);
-        }
-        else if(sum%5==0)
-        {
-            System.out.println("The Sum of is "+sum);
-        }
-
     }
 }

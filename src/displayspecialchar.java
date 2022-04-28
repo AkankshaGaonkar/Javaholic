18c1c98e18e1f99fb0142c589680ea55fc93f14b
AkankshaGaonkar <ashugaonkar2000@gmail.com>
2022-04-28 23:57:36 +0530

    displayspecialchar Program

---
+++ src/displayspecialchar.java
@@ -0,0 +1,8 @@
+public class displayspecialchar {
+    public static void main(String args[]){
+        String str ="This!is*Akanksha...%blessed+with$#beautyand&luck@.";
+        str =str.replaceAll("[a-zA-Z]","");
+        System.out.println(str);
+        }
+    }
+

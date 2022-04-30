56e5597b3bf44713c5f03e7253b24b8ea87960b2
AkankshaGaonkar <ashugaonkar2000@gmail.com>
2022-04-30 21:54:42 +0530

    RemoveSpecialChar Program

--- src/RemoveSpecialChar.java
+++ src/RemoveSpecialChar.java
@@ -1,7 +1,7 @@
-public class removespecialchar {
+public class RemoveSpecialChar {
     public static void main(String args[]){
         String str ="This!is*Akanksha...%blessed+with$#beautyand&luck@.";
-        str =str.replaceAll("[^a-zA-Z0-9]","");
+        str =str.replaceAll("[^a-zA-Z]","");
         System.out.println(str);
     }
 }

252b82b579e68ef1f958e940fd6e2cec607c8b27
AkankshaGaonkar <ashugaonkar2000@gmail.com>
2022-05-05 05:21:42 +0530

    StartsAndEnds Program

---
+++ src/StartsAndEnds.java
@@ -0,0 +1,8 @@
+public class StartsAndEnds {
+    public static void main (String args[]){
+        String str ="programmer@gmail.com";
+        System.out.println(str.startsWith("programmer"));
+        System.out.println(str.endsWith("gmail.com"));
+
+    }
+}

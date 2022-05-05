3054f6f901f3a5125fba335a75ae377a87c11114
AkankshaGaonkar <ashugaonkar2000@gmail.com>
2022-05-05 16:27:56 +0530

    BinaryOrNot Program

---
+++ src/BinaryOrNot.java
@@ -0,0 +1,10 @@
+public class BinaryOrNot {
+    public static void main(String args[]){
+        int b=1000110;
+        String str=b+"";
+        System.out.println(str.matches("[01]+"));
+
+
+
+    }
+}

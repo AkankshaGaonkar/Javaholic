66b7838326bf30b19b8515c09e4671f4a1fbd362
AkankshaGaonkar <ashugaonkar2000@gmail.com>
2022-05-05 16:35:20 +0530

    HexaDecimalOrNot Program

--- src/HexaDecimalOrNot.java
+++ src/HexaDecimalOrNot.java
@@ -2,9 +2,5 @@ public class HexaDecimalOrNot {
     public static void main(String args[]){
         String hexadeci="23DFEB";
         System.out.println(hexadeci.matches("[0-9A-F]+"));
-
-
-
-
     }
 }

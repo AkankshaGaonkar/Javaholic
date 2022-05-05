ce4543de5d1f71ce89cf1327363a02334c331c7c
AkankshaGaonkar <ashugaonkar2000@gmail.com>
2022-05-05 18:18:18 +0530

    Date Program

---
+++ src/Date.java
@@ -0,0 +1,7 @@
+public class Date {
+    public static void main(String args[]){
+        String d="17/08/2000";
+        System.out.println(d.matches("^(3[01]|[12][0-9]|0[1-9])/(1[0-2]|0[1-9])/[0-9]{4}"));
+
+    }
+}

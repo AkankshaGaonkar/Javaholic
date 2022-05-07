cfd8769d372ec3c9617a6393c8aec14ea9f42c87
AkankshaGaonkar <ashugaonkar2000@gmail.com>
2022-05-07 16:09:17 +0530

    SumOfEle Program

---
+++ src/SumOfEle.java
@@ -0,0 +1,11 @@
+public class SumOfEle {
+    public static void main(String args[]){
+        int sum=0;
+        int arr[]={3,9,7,8,12,6,15,5,4,10};
+        for(int i=0;i<arr.length;i++)
+        {
+            sum=sum+arr[i];
+        }
+        System.out.println(sum);
+    }
+}

eeda3ff5b4fe71a8aa73acd47dde5320af1cc2be
AkankshaGaonkar <ashugaonkar2000@gmail.com>
2022-05-07 16:09:45 +0530

    Pattern3 Program

---
+++ src/Pattern3.java
@@ -0,0 +1,18 @@
+public class Pattern3 {
+    public static void main(String args[])
+    {
+        int count=0;
+        for (int i =1; i <= 5; i++) {
+            for (int j = 1; j <= 5; j++)
+            {
+                ++count;
+                System.out.print(count+" ");
+            }
+            System.out.println("");
+        }
+    }
+}
+
+
+
+

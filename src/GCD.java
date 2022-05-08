90188496bc4ee1a14d312e19c34d57c0057b9f24
AkankshaGaonkar <ashugaonkar2000@gmail.com>
2022-05-08 13:37:20 +0530

    GCD Program

---
+++ src/GCD.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class GCD {
+    static int gcd(int m,int n)
+    {
+        while(m!=n)
+        {
+            if(m>n)
+            {
+                m=m-n;
+            }
+            else
+            {
+                n=n-m;
+            }
+        }
+        return(n);
+    }
+    public static void main(String args[]){
+        System.out.println(gcd(35,56));
+    }
+}

000e21188272830cd38b3e61ec2fe9c5930def79
AkankshaGaonkar <ashugaonkar2000@gmail.com>
2022-05-09 05:05:21 +0530

    CopingAnArray Program

---
+++ src/CopingAnArray.java
@@ -0,0 +1,15 @@
+public class CopingAnArray {
+    public static void main(String args[]){
+        int A[]={8,6,10,9,2,15,7,13,14,11};
+        int B[]=new int[10];
+        for(int i =0;i<A.length; i++)
+        {
+            B[i]=A[i];
+        }
+        for(int i=0;i<A.length;i++) {
+            System.out.print(B[i]+" ");
+        }
+    }
+}
+
+

1db8362969ecd9769934005db3792ae92c0d460d
AkankshaGaonkar <ashugaonkar2000@gmail.com>
2022-05-08 14:32:48 +0530

    MaxEleInArray Program

---
+++ src/MaxEleInArray.java
@@ -0,0 +1,18 @@
+public class MaxEleInArray {
+    static int max(int A[])
+    {
+        int max=A[0];
+        for(int i=1;i<A.length;i++)
+        {
+            if(max<A[i])
+            {
+                max=A[i];
+            }
+        }
+        return(max);
+    }
+    public static void main(String args[]){
+        int A[]={8,3,15,9,77};
+        System.out.println(max(A));
+    }
+}

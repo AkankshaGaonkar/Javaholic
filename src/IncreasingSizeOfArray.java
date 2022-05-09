62bfc0a42b405082fe089a8543328229935a024b
AkankshaGaonkar <ashugaonkar2000@gmail.com>
2022-05-09 06:02:01 +0530

    IncreasingSizeOfArray Program

---
+++ src/IncreasingSizeOfArray.java
@@ -0,0 +1,15 @@
+public class IncreasingSizeOfArray {
+    public static void main(String args[]){
+        int A[]={8,6,10,9,2};
+        System.out.println(A.length);
+        int B[]=new int[2*A.length];
+        for(int i =0;i<A.length; i++)
+        {
+            B[i]=A[i];
+        }
+        A=B;
+
+        System.out.println(A.length);
+    }
+}
+

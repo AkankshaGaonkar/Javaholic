026bec9749e11606f93789d2dc0daa0f9d0f25b9
AkankshaGaonkar <ashugaonkar2000@gmail.com>
2022-05-15 12:16:26 +0530

    ExampleforabstractClass

---
+++ src/egForAbstractClass.java
@@ -0,0 +1,47 @@
+abstract class shape
+{
+    abstract public int perimeter();
+    abstract public int area();
+}
+class circle extends shape{
+    int radius;
+    public int area()
+    {
+        return radius*radius;
+    }
+    public int perimeter()
+    {
+        return 2*radius;
+    }
+
+}
+class rect extends shape
+{
+    int length, breath;
+    public int area()
+    {
+        return (length*breath);
+    }
+    public int perimeter()
+    {
+        return (2*(length+breath));
+    }
+
+
+}
+
+public class egForAbstractClass {
+    public static void  main(String args[]){
+        circle c= new circle();
+         rect r = new rect();
+         r.length=10;
+         r.breath=2;
+         c.radius=3;
+         shape s =c;//dynamic mtd dispatch
+         System.out.println(s.area());
+        System.out.println(c.perimeter());
+        System.out.println(r.area());
+        System.out.println(r.perimeter());
+    }
+
+}

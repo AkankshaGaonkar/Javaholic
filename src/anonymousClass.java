dbe8456da9414374c5a47c5eae38ac2f07aef32d
AkankshaGaonkar <ashugaonkar2000@gmail.com>
2022-05-15 19:56:15 +0530

    anonymousClass

--- src/anonymousClass.java
+++ src/anonymousClass.java
@@ -6,14 +6,21 @@ class Outerr
 {
     public void meth()
     {
-        exam m = new exam() { //anonymous class
+        /*exam m = new exam() { //anonymous class
             @Override
             public void display() {
                 System.out.println("hello");
             }
         };
-        m.display();
+        m.display();*/
+        new exam(){public void display(){System.out.println("hello");}}.display();
+
     }
 }
 public class anonymousClass {
+    public static void main(String args[])
+    {
+        Outerr o = new Outerr();
+        o.meth();
+    }
 }

9e11512be20ce9d027317498dc7b24135b6e274d
AkankshaGaonkar <ashugaonkar2000@gmail.com>
2022-05-15 11:20:15 +0530

    abstractClass

---
+++ src/abstractClass.java
@@ -0,0 +1,28 @@
+abstract  class Superr
+{
+    Superr() //constructor
+    {
+        System.out.println("Super");
+    }
+    void meth1(){
+        System.out.println("Super Method1");
+    }
+    abstract void meth2();
+}
+class Subb extends Superr
+{
+    void meth2()
+    {
+        System.out.println("Sub Method2");
+    }
+}
+public class abstractClass {
+    public static void main(String args[])
+    {
+        Superr s =new Subb();//you can only create the obj of the subclass.Mainly abstract class is used for inheritance so that another class can override the mtds and create obj and functions to execute
+        s.meth1();
+        s.meth2();
+
+    }
+}
+

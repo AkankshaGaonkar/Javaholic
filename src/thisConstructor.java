6642c08351c1dcce58abb6611bc5b8c8fcdd46d2
AkankshaGaonkar <ashugaonkar2000@gmail.com>
2022-05-16 18:21:26 +0530

    thisconstructor

--- src/thisConstructor.java
+++ src/thisConstructor.java
@@ -1,23 +1,35 @@
-class rectangle{
-    int length;
-    int breath;
-    rectangle(int length, int breath)
+class rectangle1 {
+    int length=100;
+    int breath=200;
+    rectangle1()
+    {
+        int length;
+        int breath;
+        length=16;
+        breath=13;
+        System.out.println("length="+this.length);
+        System.out.println("breath="+this.breath);
+
+    }
+
+    rectangle1(int length, int breath)
     {
         this.length=length;//to avoid name conflict that is parameter and property have same name so this constructor is used to resolve this issue
         //this gives ref to current obj
         this.breath=breath;
     }
-    boolean display()
+    void display()
     {
-        System.out.println("length"+this.length);
-        System.out.println("breath"+this.breath);
-        return false;
+        System.out.println("length="+this.length);
+        System.out.println("breath="+this.breath);
+
     }
 }
 public class thisConstructor {
     public static void main(String args[]) {
-        rectangle r = new rectangle(2,5);
-        System.out.println(r.display());
+        rectangle1 rr = new rectangle1();
 
+        rectangle1 r = new rectangle1(2,5);
+        r.display();
     }
 }

9813d2c27568690bb8987b07c77816c0936df1b3
AkankshaGaonkar <ashugaonkar2000@gmail.com>
2022-05-17 09:23:32 +0530

    Creating Object And Inner Class

---
+++ src/practice.java
@@ -0,0 +1,29 @@
+class akanksha
+{
+    void careerplanning()
+    {
+        System.out.println("Getting settled in Abroad");
+
+    }
+    static class prasad
+    {
+        void futureplanning()
+        {
+            System.out.println("Having an own house and a car");
+        }
+        void careerplanning()
+        {
+            System.out.println("Getting settled in Abroad");
+        }
+
+    }
+    public static void main(String[] ashu) {
+        akanksha a = new akanksha();// name of the class with which we want to create an obj
+        //any obj name
+        //new is the keyword
+        //class name;parenthesis bec of the constructor
+        a.careerplanning();
+        prasad p = new prasad();
+        p.futureplanning();
+    }
+}
